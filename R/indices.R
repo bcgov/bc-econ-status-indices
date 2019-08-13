525e22d1ff9bbcf0deead6d223068254190a42f1
Noushin Nabavi <nabavinoushin@gmail.com>
2019-08-13 14:29:24 -0700

    update index for rural areas

--- R/indices.R
+++ R/indices.R
@@ -32,11 +32,15 @@ synthetic_data_linkage <- synthetic_data %>%
 tax_data_linkage <- tax_data %>%
   select(`year`, `postal|area`, `level|of|geo`, `place|name|geo`, `total|income|median|total`)
 
-linked_data <- inner_join(tax_data_linkage, synthetic_data_linkage, by = "postal|area")
+linked_data <- left_join(tax_data_linkage, synthetic_data_linkage, by = "postal|area")
+
+# check whether data has NA's after left_join
+naniar::miss_var_summary(linked_data)
 
 #-------------------------------------------------------------------------------
 
 # Generate quintile range of income for urban areas
+##  Level of Geography (L.O.G.): 61 = Area: Census Tract
 
 urban_index <- linked_data %>%
   group_by(`year`) %>%
@@ -44,34 +48,50 @@ urban_index <- linked_data %>%
   mutate(UQs =  ntile(`total|income|median|total`, 5)) %>%
   ungroup()
 
+# remove NA's in the studyid column
+urban_index %>%
+  drop_na(studyid)
+
+
 #-------------------------------------------------------------------------------
 
 # Generate quintile range of income for rural areas (composite)
 
+## Level of Geography (L.O.G.): 09 = Postal Area: Rural Communities (Not in City)
+
 rural_index_rc <- linked_data %>%
   group_by(`year`) %>%
-  filter(`level|of|geo` == 9) %>% # geo level 9 denotes rural communities
+  filter(`level|of|geo` == 9) %>%
   mutate(RQ_a =  ntile(`total|income|median|total`, 5)) %>%
   ungroup()
 
+## Level of Geography (L.O.G.): 06 = Postal Area: Rural Postal Code Areas (Within City)
+
 rural_index_rpc <- linked_data %>%
   group_by(`year`) %>%
-  filter(`level|of|geo` ==  6) %>% # geo level 6 denote rural postal codes
+  filter(`level|of|geo` ==  6) %>%
   mutate(RQ_b =  ntile(`total|income|median|total`, 5)) %>%
   ungroup()
 
+## Level of Geography (L.O.G.): 21 = Area: Census Division
 rural_index_cd <- linked_data %>%
   group_by(`year`) %>%
-  filter(`level|of|geo` == 21) %>% # geo levels 21 denotes census division
+  filter(`level|of|geo` == 21) %>%
   mutate(RQ_c =  ntile(`total|income|median|total`, 5)) %>%
   ungroup()
 
-# merge indices together
+# check duplicates in `postal|area`
+rural_index_rc %>%
+  filter(year == 2015)
+
+
+# merge indices together and drop NA's in studyid column
 rural_index <- plyr::rbind.fill(rural_index_rc, rural_index_rpc, rural_index_cd, rural_index_cd) %>%
-  mutate(RQs = c(na.omit(RQ_a),na.omit(RQ_b), na.omit(RQ_c)))
+  mutate(RQs = c(na.omit(RQ_a),na.omit(RQ_b), na.omit(RQ_c))) %>%
+  drop_na(studyid)
 
 # clean out additional columns
 drop.cols <- c("RQ_a", "RQ_b", "RQ_c")
-rural_index %>% select(-one_of(drop.cols))
+rural_index <- rural_index %>% select(-one_of(drop.cols))
 
 
